6b5d3d584b405ca1773f2cb03e0d2750c9a05e18
Jordi Chorro Gimeno <jordichorrog96@gmail.com>
2024-10-15 20:10:25 +0200

    Initial commit

---
+++ app/src/main/java/com/jordi/chorro/tema3_actividad3/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.t3a3.jordichorro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jordi.chorro.tema3_actividad3.R
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+
+    }
+}
---
+++ app/src/main/java/com/jordi/chorro/tema3_actividad3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.t3a3.jordichorro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jordi.chorro.tema3_actividad3.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
